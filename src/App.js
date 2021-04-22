a02038e6ffafaf8366ad04448d3fb31be66f2dc1
Dani <dani8439@aol.com>
2021-04-22 16:13:54 -0400

    testing componentDidMount that routes are working.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from 'react';
 
 class App extends React.Component {
+
+  componentDidMount() {
+    fetch('http://localhost:3000/api/v1/collections/1/pieces')
+    .then(resp => resp.json())
+    .then(resp => console.log(resp))
+  }
+
   render(){
     return (
       <div className="App">
-        <App />
+        Hello World!
       </div>
     );
   }
