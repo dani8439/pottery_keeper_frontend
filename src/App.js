36d024c7e110633bdff4b4dc4612e5cca1db3fef
Dani <dani8439@aol.com>
2021-04-23 10:39:36 -0400

    installed redux, react-redux, thunk, and react-router-dom.

--- src/App.js
+++ src/App.js
@@ -2,11 +2,11 @@ import React from 'react';
 
 class App extends React.Component {
 
-  componentDidMount() {
-    fetch('http://localhost:3000/api/v1/collections/1/pieces')
-    .then(resp => resp.json())
-    .then(resp => console.log(resp))
-  }
+  // componentDidMount() {
+  //   fetch('http://localhost:3000/api/v1/collections/1/pieces')
+  //   .then(resp => resp.json())
+  //   .then(resp => console.log(resp))
+  // }
 
   render(){
     return (
