2cb04dcee2bdfa2ac37cf804d7c8e670387a3e81
Dani <dani8439@aol.com>
2021-04-23 10:46:50 -0400

    setting up store and provider in index.js.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore, applyMiddleware} from 'redux';
+import thunk from 'redux-thunk';
+import { Provider } from 'react-redux';
+
+
 
 import App from './App';
 
@@ -8,6 +13,4 @@ ReactDOM.render(
     <App />,
   document.getElementById('root')
 );
-
-/// test. 
 
