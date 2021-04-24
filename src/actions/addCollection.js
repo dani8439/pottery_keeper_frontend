b8de334fe09d939d024684ef531b712b1387926b
Dani <dani8439@aol.com>
2021-04-24 13:11:07 -0400

    set up handleChange on form, starting to set up action for adding a collection.

---
+++ src/actions/addCollection.js
@@ -0,0 +1,3 @@
+export const addCollection = (data) => {
+    
+}
--- src/components/CollectionInput.js
+++ src/components/CollectionInput.js
@@ -2,15 +2,34 @@ import React from 'react';
 
 class CollectionInput extends React.Component {
 
+    // controlled form
+
+    state = {
+        name: '', 
+        main_image: ''
+    }
+
+    handleChange = (event) => {
+        this.setState({
+            [event.target.name]: event.target.value
+        })
+
+    }
+
+    handleSubmit = () => {
+        
+    }
+
     render() {
         return (
             <div>
-                <form>
+                <form onSubmit={this.handleSubmit}>
                     <label>Collection Name:</label>
-                    <input type="text" placeholder="Name"/><br></br>
+                    <input type="text" placeholder="Name" name="name" onChange={this.handleChange} value={this.state.name}/><br></br>
                     <br></br>
                     <label>Main Image:</label>
-                    <input type="text" placeholder="Main Image"/>
+                    <input type="text" placeholder="Main Image" name="main_image" onChange={this.handleChange} value={this.state.main_image}/><br></br>
+                    <input type="submit"/>
                 </form>
             </div>
         )
