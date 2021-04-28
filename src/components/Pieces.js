76e2729dc36dd788d1fa172b7fedef5a1307df1e
Dani <dani8439@aol.com>
2021-04-28 10:01:57 -0400

    added check so that first time props come through in Pieces component, does not render an error.

--- src/components/Pieces.js
+++ src/components/Pieces.js
@@ -3,13 +3,19 @@ import React from 'react';
 // functional component because sending down props through PieceInput. And just rendering out the pieces. 
 
 const Pieces = (props) => {
+    console.log(props.pieces)
+    debugger;
 
 
     return (
+        // {/* Add in check because first time props come through, it's undefined*/}
         <div>
-            {/*{props.pieces.map(piece => 
-                <li key={piece.id}>{piece.pattern_name} {piece.piece_name} {piece.image_url} {piece.quantity}</li>
-            )} */}
+            {props.pieces && props.pieces.map(piece => 
+            <div key={piece.id}>
+                <h3>{piece.pattern_name} {piece.piece_name} - {piece.quantity}</h3>
+                <img src={piece.image_url}/>
+            </div>
+            )}
         </div>
     )
 }
