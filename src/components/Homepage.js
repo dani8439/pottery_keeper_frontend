8e4da9833f4cb28252bed994260baa77fd329819
Dani <dani8439@aol.com>
2021-05-26 16:20:02 -0400

    homepage is presently wrong.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Homepage = (props) => {
     return (
         <div>
-            <img src="./Images/screenshot1.png"></img>
+            <img src="../Images/screenshot1.jpg"></img>
         </div>
     )
 }
