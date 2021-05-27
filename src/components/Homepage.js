65c8fd003b9fe04de4a9e0a11c4dbc35e2eb6b77
Dani <dani8439@aol.com>
2021-05-27 13:57:32 -0400

    switched images for homepage.

--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -4,7 +4,7 @@ const Homepage = (props) => {
     return (
         <div>
             <h1>An Image</h1>
-            <img src="Images/screenshot1.jpg" alt="some pottery"/>
+            <img src="../Images/screenshot1.jpg" alt="some pottery"/>
         </div>
     )
     
