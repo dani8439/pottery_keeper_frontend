37d8975b8beb72040535df689c6d5c5db804a218
Dani <dani8439@aol.com>
2021-05-28 13:14:42 -0400

    tweaking.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = (props) => {
     return (
         <div className="footer">
-            <p>Pottery Keeper: Helping to Keep Your Collecting Organized.</p>
+            <p>Pottery Keeper: For the anal retentive obsessive.</p>
             <a href="https://www.ebay.co.uk/sch/i.html?_from=R40&_nkw=emma+bridgewater&_sacat=0&_sop=10">Oopsies</a>
 
         </div>
--- src/components/Homepage.js
+++ src/components/Homepage.js
@@ -6,7 +6,8 @@ const Homepage = (props) => {
             {/* <img src="https://cdn.shopify.com/s/files/1/1987/6031/files/New_Glass_Vases_Block_Banner_720x.jpg?v=1621678813"/> */}
             <img src="https://cdn.shopify.com/s/files/1/1987/6031/files/EBPatternHouse-291997_PR_2048x2048.jpg?v=1521042593"/>
             <br></br>
-            <p>Some text about why this app is useful.</p>
+            <p>Pottery Keeper is a visual diary to keep collections organized. Sort your collections according to preference, however you wish.</p>
+            <br></br>
         </div>
     ) 
     
