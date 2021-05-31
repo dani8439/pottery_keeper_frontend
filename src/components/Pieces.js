68866a8dd801d4d91f53aca66c352a00bd1b9c36
Dani <dani8439@aol.com>
2021-05-31 16:48:31 -0400

    switching pieces over to class component from functional, making sure code isn't broken.

--- src/components/Pieces.js
+++ src/components/Pieces.js
@@ -5,30 +5,34 @@ import { editPiece } from '../actions/editPiece';
 
 // functional component because sending down props through PieceInput. And just rendering out the pieces.
 
-const Pieces = (props) => {
+class Pieces extends React.Component {
     // console.log(props.pieces)
 
 
     // because functional component, have to define with const keyword.
-    const handleDelete = (piece) => {
-        props.deletePiece(piece.id, piece.collection_id)
+    handleDelete = (piece) => {
+        this.state.deletePiece(piece.id, piece.collection_id)
     }
 
 
-    return (
-        // {/* Add in check because first time props come through, it's undefined. With react, when you refresh page, state and store are cleared out.*/}
-        <div>
-            {props.pieces && props.pieces.map(piece =>
-            <div className="card" key={piece.id}>
-                <h3>{piece.pattern_name} {piece.piece_name} - {piece.quantity}</h3>
-                <img src={piece.image_url} alt="pieces" className="smallImage"/>
-                <br></br>
-                <button onClick={() => props.onEdit(piece)}>EDIT</button>
-                <button onClick={() => handleDelete(piece)}>DELETE</button>
+    render() {
+        return (
+            // {/* Add in check because first time props come through, it's undefined. With react, when you refresh page, state and store are cleared out.*/}
+            <div>
+                {this.props.pieces && this.props.pieces.map(piece =>
+                <div className="card" key={piece.id}>
+                    <h3>{piece.pattern_name} {piece.piece_name} - {piece.quantity}</h3>
+                    <img src={piece.image_url} alt="pieces" className="smallImage"/>
+                    <br></br>
+                    <button onClick={() => this.props.onEdit(piece)}>EDIT</button>
+                    <button onClick={() => this.props.handleDelete(piece)}>DELETE</button>
+                </div>
+                )}
             </div>
-            )}
-        </div>
-    )
+        )
+
+    }
+
 }
 
 export default connect(null, { deletePiece, editPiece })(Pieces)
