2cf7edd4fe6820f2efac2d9cc02c1d4cf2dd8047
Dani <dani8439@aol.com>
2021-05-31 18:02:41 -0400

    search bar not working.

--- src/components/CollectionEdit.js
+++ src/components/CollectionEdit.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { editCollection } from '../actions/editCollection';
 
+// class Component
 
 class CollectionEdit extends React.Component {
 
--- src/components/Pieces.js
+++ src/components/Pieces.js
@@ -3,15 +3,43 @@ import { connect } from 'react-redux';
 import { deletePiece } from '../actions/deletePiece';
 import { editPiece } from '../actions/editPiece';
 
-// functional component because sending down props through PieceInput. And just rendering out the pieces.
+// switched over to Class Component so can manipulate state and search for pieces? 
 
 class Pieces extends React.Component {
-    // console.log(props.pieces)
+    state = {
+        // pieces: [],
+        search: ""
+    }
 
 
-    // because functional component, have to define with const keyword.
+
     handleDelete = (piece) => {
         this.state.deletePiece(piece.id, piece.collection_id)
+    }
+
+    handleInputChange = (e) => {
+        console.log(this.state)
+        const search = e.target.value 
+        this.setState({search: search})
+    }
+
+    makePieceCards() {
+        let displayedPieces = this.props.pieces
+        // console.log(this.state.seach)
+        if(this.state.search){
+            displayedPieces = this.state.pieces && this.state.pieces.filter((piece) => piece.piece_name.toLowerCase().includes(this.state.search.toLowerCase()))
+        }
+
+        return displayedPieces && displayedPieces.map(piece =>
+            <div className="card" key={piece.id}>
+                <h3>{piece.pattern_name} {piece.piece_name} - {piece.quantity}</h3>
+                <img src={piece.image_url} alt="pieces" className="smallImage"/>
+                <br></br>
+                <button onClick={() => this.state.onEdit(piece)}>EDIT</button>
+                <button onClick={() => this.state.handleDelete(piece)}>DELETE</button>
+            </div>
+            )
+
     }
 
 
@@ -19,15 +47,8 @@ class Pieces extends React.Component {
         return (
             // {/* Add in check because first time props come through, it's undefined. With react, when you refresh page, state and store are cleared out.*/}
             <div>
-                {this.props.pieces && this.props.pieces.map(piece =>
-                <div className="card" key={piece.id}>
-                    <h3>{piece.pattern_name} {piece.piece_name} - {piece.quantity}</h3>
-                    <img src={piece.image_url} alt="pieces" className="smallImage"/>
-                    <br></br>
-                    <button onClick={() => this.props.onEdit(piece)}>EDIT</button>
-                    <button onClick={() => this.props.handleDelete(piece)}>DELETE</button>
-                </div>
-                )}
+                <input type="text" placeholder="Search for a piece..." onChange={this.handleInputChange} /><br></br>
+                {this.makePieceCards()}
             </div>
         )
 
