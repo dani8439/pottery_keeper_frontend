2687cd64ee9da3ff50e4a94ff10c7fd8151976b3
Dani <dani8439@aol.com>
2021-06-01 14:35:14 -0400

    Switched Pieces from functional component to class component.

--- src/components/Pieces.js
+++ src/components/Pieces.js
@@ -3,32 +3,40 @@ import { connect } from 'react-redux';
 import { deletePiece } from '../actions/deletePiece';
 import { editPiece } from '../actions/editPiece';
 
-// functional component because sending down props through PieceInput. And just rendering out the pieces.
 
-const Pieces = (props) => {
-    // console.log(props.pieces)
+class Pieces extends React.Component {
+    state = {
+        pieces: []
 
-
-    // because functional component, have to define with const keyword.
-    const handleDelete = (piece) => {
-        props.deletePiece(piece.id, piece.collection_id)
     }
 
 
-    return (
+    handleDelete = (piece) => {
+        this.props.deletePiece(piece.id, piece.collection_id)
+    }
+
+    makePieceCards() {
         // {/* Add in check because first time props come through, it's undefined. With react, when you refresh page, state and store are cleared out.*/}
-        <div>
-            {props.pieces && props.pieces.map(piece =>
+        return this.props.pieces && this.props.pieces.map(piece => 
             <div className="card" key={piece.id}>
                 <h3>{piece.pattern_name} {piece.piece_name} - {piece.quantity}</h3>
                 <img src={piece.image_url} alt="pieces" className="smallImage"/>
                 <br></br>
-                <button onClick={() => props.onEdit(piece)}>EDIT</button>
-                <button onClick={() => handleDelete(piece)}>DELETE</button>
+                <button onClick={() => this.props.onEdit(piece)}>EDIT</button>
+                <button onClick={() => this.handleDelete(piece)}>DELETE</button>
             </div>
-            )}
-        </div>
-    )
+            )
+    }
+
+    render() {
+        return (
+            
+            <div>
+                {this.makePieceCards()}
+            </div>
+        )
+
+    }
 }
 
 export default connect(null, { deletePiece, editPiece })(Pieces)
